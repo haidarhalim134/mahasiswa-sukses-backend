787c34d0ecf9b14bdbb409884d6b104a369253f1
haidarhalim134 <haidarah134@gmail.com>
2026-06-15 20:27:09 +0700

    feat: add password validator for forgot password page and update password through profile

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -65,6 +65,20 @@ class UpdatePasswordRequest(BaseModel):
     access_token: str
     password: str = Field(min_length=8)
 
+    @field_validator("password")
+    def validate_password_complexity(cls, v):
+        # at least one letter: (?=.*[A-Za-z])
+        # at least one digit: (?=.*\d)
+        # at least one special character: (?=.*[@$!%*#?&])
+        # at least 8 char long: {8,}
+        pattern = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+        
+        if not re.match(pattern, v):
+            raise ValueError(
+                "Password must contain at least one letter, one number, and one special character"
+            )
+        return v
+
 class LoginResponse(BaseModel):
     access_token: str
     refresh_token: str
--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,8 +1,9 @@
 from datetime import date
+import re
 from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class UserStats(BaseModel):
     total_points: int
@@ -33,6 +34,20 @@ class ProfileUpdate(BaseModel):
     nim: Optional[str] = None
     birth_date: Optional[date] = None
     password: Optional[str] = Field(None, min_length=8)
+
+    @field_validator("password")
+    def validate_password_complexity(cls, v):
+        # at least one letter: (?=.*[A-Za-z])
+        # at least one digit: (?=.*\d)
+        # at least one special character: (?=.*[@$!%*#?&])
+        # at least 8 char long: {8,}
+        pattern = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+        
+        if v != None and not re.match(pattern, v):
+            raise ValueError(
+                "Password must contain at least one letter, one number, and one special character"
+            )
+        return v
 
 class SettingsUpdate(BaseModel):
     notifications: Optional[bool] = None
